0f06895e5edd9803a22b768742fd107525288ff9
Luigi Feola <luigifeola.work@gmail.com>
2023-08-24 15:39:27 +0200

    Remove Kilobot_behaviour folder because duplicate of the kilobot_ALF_dhtf.c behaviour

--- Kilobot_behaviour/distribution_functions.h
+++
@@ -1,9 +0,0 @@
-/*@author cdimidov*/
-
-double wrapped_cauchy_ppf (const double c);
-
-double uniform_distribution (double a , double b);
-
-double exponential_distribution (double lambda);
-
-int levy (const double c, const double alpha);
